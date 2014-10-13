fc30bbcfacd9b8080c0399a5685108708b01f9f3
Nicholas Ingolia <nick@ingolia.org>
2014-10-13 02:13:09 +0200

    Added SeqLocMap to support heuristic overlap detection with different sequence names

--- seqloc/src/Bio/SeqLoc/LocMap.hs
+++ seqloc/src/Bio/SeqLoc/LocMap.hs
@@ -2,6 +2,7 @@
 module Bio.SeqLoc.LocMap
        where
 
+import qualified Data.HashMap.Strict as HM
 import qualified Data.Vector as V
 
 import qualified Bio.SeqLoc.Location as Loc
@@ -12,6 +13,8 @@ import qualified Bio.SeqLoc.ShiftedVector as ShV
 
 data LocMap a = LocMap { binSize :: !Pos.Offset,
                          bins :: !(ShV.ShiftedVector [a]) } deriving (Show)
+
+data SeqLocMap a = SeqLocMap { slmBinSize :: !Pos.Offset, locmaps :: !(HM.HashMap SeqLabel (LocMap a)) } deriving (Show)
 
 emptyLM :: Pos.Offset -> LocMap a
 emptyLM bsz = LocMap { binSize = bsz, bins = ShV.empty }
@@ -27,3 +30,15 @@ locBins l lm = let (start, end) = Loc.bounds l
                    binlow = fromIntegral $ start `div` binSize lm
                    binhigh = fromIntegral $ end `div` binSize lm
                in [binlow..binhigh]
+
+emptySLM :: Pos.Offset -> SeqLocMap a
+emptySLM bsz = SeqLocMap { slmBinSize = bsz, locmaps = HM.empty }
+
+insertSeqLoc :: (Loc.Location l) => OnSeq l -> a -> SeqLocMap a -> SeqLocMap a
+insertSeqLoc sl x slm0 = let lm0 = HM.lookupDefault (emptyLM $ slmBinSize slm0) (onSeqLabel sl) (locmaps slm0)
+                             lm' = insertLoc (unOnSeq sl) x lm0
+                         in slm0 { locmaps = HM.insert (onSeqLabel sl) lm' (locmaps slm0) }
+
+querySeqLoc :: (Loc.Location l) => OnSeq l -> SeqLocMap a -> [a]
+querySeqLoc sl slm = maybe [] (queryLoc (unOnSeq sl)) $
+                     HM.lookup (onSeqLabel sl) (locmaps slm)
